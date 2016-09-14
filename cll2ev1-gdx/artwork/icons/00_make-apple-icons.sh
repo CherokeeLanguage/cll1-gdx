6d6675da59c3536eca42ae847c0c88460e34f989
mjoyner <mjoyner@vbservices.net>
2016-09-13 20:36:35 -0400

    ignore generated files

--- cll2ev1-gdx/artwork/icons/00_make-apple-icons.sh
+++ cll2ev1-gdx/artwork/icons/00_make-apple-icons.sh
@@ -12,14 +12,16 @@ cd "$(dirname "$0")"
 rm icon.png 2> /dev/null || true
 inkscape -z -d 300 -C --export-background=white --export-background-opacity=1 -e=icon.png icon.svg
 
-mkdir deskcons || true 2> /dev/null
+mkdir deskcons 2> /dev/null || true 
 
 gm convert -filter ${FILTER} icon.png -resize 128x128 deskcons/icon-128.png
 gm convert -filter ${FILTER} icon.png -resize 64x64 deskcons/icon-64.png
 gm convert -filter ${FILTER} icon.png -resize 32x32 deskcons/icon-32.png
 gm convert -filter ${FILTER} icon.png -resize 16x16 deskcons/icon-16.png
 
-mkdir applecons || true 2> /dev/null
+cp -v deskcons/*.png ../../android/assets/icons/
+
+mkdir applecons 2> /dev/null || true 
 
 gm convert -filter ${FILTER} icon.png -resize 72x72 applecons/Icon-72.png
 gm convert -filter ${FILTER} icon.png -resize 144x144 applecons/Icon-72@2x.png
@@ -34,3 +36,7 @@ gm convert -filter ${FILTER} icon.png -resize 240x240 applecons/Icon-120@2x.png
 
 gm convert -filter ${FILTER} icon.png -resize 152x152 applecons/Icon-152.png
 gm convert -filter ${FILTER} icon.png -resize 304x304 applecons/Icon-152@2x.png
+
+cp -v applecons/*.png ../../ios/data/
+
+
