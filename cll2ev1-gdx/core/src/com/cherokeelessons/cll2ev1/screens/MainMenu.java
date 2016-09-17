8a0be4a4117d3b2d9c06a6d1e4d24738feae95b4
mjoyner <mjoyner@vbservices.net>
2016-09-16 23:54:11 -0400

    working menu

--- cll2ev1-gdx/core/src/com/cherokeelessons/cll2ev1/screens/MainMenu.java
+++ cll2ev1-gdx/core/src/com/cherokeelessons/cll2ev1/screens/MainMenu.java
@@ -1,6 +1,7 @@
 package com.cherokeelessons.cll2ev1.screens;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.cherokeelessons.cll2ev1.CLL2EV1;
@@ -12,7 +13,19 @@ public class MainMenu extends AbstractScreen {
 		setClearColor(Color.WHITE);
 		
 		Table menu = new Table(skin);
-		menu.add(new TextButton("Menu Item", skin));
+		menu.setFillParent(true);
+		menu.row();
+		menu.add(new Label("Cherokee Language Lessons Vol. 1", skin));
+		menu.row();
+		menu.add(new TextButton("New Game - ᎢᏤ ᏗᏁᎶᏗᎢ", skin));
+		menu.row();
+		menu.add(new TextButton("High Scores - ᏬᏍᏓ ᏗᏎᏍᏗ", skin));
+		menu.row();
+		menu.add(new TextButton("Options - ᎠᏑᏰᏍᏗᎢ", skin));
+		menu.row();
+		menu.add(new TextButton("About - ᎢᎸᏢ", skin));
+		menu.row();
+		menu.add(new TextButton("Quit - ᎠᏑᎶᎪᏍᏗ", skin));
 		menu.pack();
 		
 		stage.addActor(menu);
