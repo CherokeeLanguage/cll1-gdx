149e86a3498076bd9358f3e982b169acc9ec3b16
mjoyner <mjoyner@vbservices.net>
2016-09-21 22:38:04 -0400

    having lists of subdirectories by directory is useful.

--- cll2ev1-gdx/android/assets/make-files-txt.sh
+++ cll2ev1-gdx/android/assets/make-files-txt.sh
@@ -17,14 +17,22 @@ cwd="$(pwd)"
 
 if [ ! -f .gitignore ]; then touch .gitignore; fi
 grep "/0_files.txt" .gitignore > /dev/null 2>&1 || echo "/0_files.txt" >> .gitignore  
+grep "/0_dirs.txt" .gitignore > /dev/null 2>&1 || echo "/0_dirs.txt" >> .gitignore
 
 echo "... $cwd"
 
 cp /dev/null 0_files.txt
+cp /dev/null 0_dirs.txt
+
 for dir in *; do
-	if [ -d "$dir" ]; then bash "$0" "$dir" || exit $?; continue; fi
+	if [ -d "$dir" ]; then
+		echo "$(basename "$dir")" >> 0_dirs.txt
+		bash "$0" "$dir" || exit $?;
+		continue;
+	fi
 	dir="$(basename "$dir")"
 	if [ "$dir" = "0_files.txt" ]; then continue; fi
+	if [ "$dir" = "0_dirs.txt" ]; then continue; fi
 	echo "$(basename "$dir")" >> 0_files.txt
 done
 
