6e3f7e1328991dab85e9bfde34bb55121c09f13d
mjoyner <mjoyner@vbservices.net>
2016-10-02 03:33:40 -0400

    fix issue with wrong cards vs correct cards and Chapter 8's variations for ᎥᏍᎩᎾ vs ᎾᏍᎩ

--- cll2ev1-gdx/artwork/card-data/audio/fix-audio.sh
+++ cll2ev1-gdx/artwork/card-data/audio/fix-audio.sh
@@ -9,12 +9,12 @@ cd "$(dirname "$0")"
 cwd="$(pwd)"
 
 for x in ???; do
-	echo "--- $x"
+	echo "--- normalize-mp3 $x/"
 	cd "$cwd"
 	cd "$x"
 	for mp3 in *.mp3; do
 		if [ ! -f "$mp3" ]; then continue; fi
-		normalize-mp3 -T 1 --bitrate "$BR" "$mp3"
+		normalize-mp3 -q -T 1 --bitrate "$BR" "$mp3"
 	done
 done
 echo "DONE"
