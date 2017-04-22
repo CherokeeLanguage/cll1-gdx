1616b56ae0a41def98eb115c256d22f3ede3b912
mjoyner <mjoyner@vbservices.net>
2017-04-22 17:43:04 -0400

    inkscape/mp3gain changes in SVG/PNG caused by inkscape issues. changes in MP3 files caused by loss of 'mp3gain' program.

--- cll1-gdx/artwork/card-data/audio/fix-audio.sh
+++ cll1-gdx/artwork/card-data/audio/fix-audio.sh
@@ -14,8 +14,9 @@ for x in ???; do
 	cd "$x"
 	for mp3 in *.mp3; do
 		if [ ! -f "$mp3" ]; then continue; fi
-		#normalize-mp3 -T 1 --bitrate "$BR" "$mp3"
-		mp3gain -q -m 6 -s i -k -a "$mp3" > /dev/null
+		normalize-mp3 -T 1 --bitrate "$BR" --mp3 "$mp3"
+		#normalize-mp3 -b -a .7 -T 1 --bitrate "$BR" "$mp3"
+		#mp3gain -q -m 6 -s i -k -a "$mp3" > /dev/null
 		#mp3gain -k -r "$mp3"
 	done
 done
